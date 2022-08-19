5681240f07184785b540a7e157bb52e2b1d3c35f
DasherDC <dccharge@163.com>
2022-08-19 13:06:00 +0800

    modification

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -1,5 +1,8 @@
 from .common import *
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = True
 ALLOWED_HOSTS = ['47.103.1.12']
